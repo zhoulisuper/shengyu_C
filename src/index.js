9c33557d83090a8325a14327652e79493d801134
19033436 <19033436@suning.com>
2020-05-21 20:57:17 +0800

    更改css的样式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "style/index.css";
 import * as serviceWorker from "./serviceWorker";
 import Router from "./route";
 
