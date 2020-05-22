d267e522fd9059c4ca4610fc01ccd01742b31a57
19033436 <19033436@suning.com>
2020-05-22 18:12:41 +0800

    开发页面布局

--- src/page/Demo/index.jsx
+++ src/page/Demo/index.jsx
@@ -1,20 +1,189 @@
 import React from "react";
-import { Button } from "antd-mobile";
-import { Link } from "react-router-dom";
-import s from "./style.module.less";
+import { TabBar } from "antd-mobile";
 
 class Demo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: "home",
+      hidden: false,
+    };
+  }
+
+  renderContent = (pageText) => {
+    return (
+      <div
+        style={{
+          backgroundColor: "white",
+          height: "100%",
+          textAlign: "center",
+        }}
+      >
+        <div style={{ paddingTop: 60 }}>
+          Clicked “{pageText}” tab， show “{pageText}” information
+        </div>
+        <p
+          style={{
+            display: "block",
+            marginTop: 40,
+            marginBottom: 20,
+            color: "#108ee9",
+          }}
+          onClick={(e) => {
+            e.preventDefault();
+            this.setState({
+              hidden: !this.state.hidden,
+            });
+          }}
+        >
+          Click to show/hide tab-bar
+        </p>
+      </div>
+    );
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className={s.container}>
-          <Button type="primary" inline size="small">
-            按钮
-          </Button>
-          <div style={{ height: "100px" }}>
-            <Link to="/demo2">demo2</Link>
-          </div>
-        </header>
+      <div style={{ position: "fixed", height: "100%", width: "100%", top: 0 }}>
+        {/* <NavBar
+          mode="light"
+          icon={<Icon type="left" />}
+          onLeftClick={() => console.log("onLeftClick")}
+          rightContent={[
+            <Icon key="0" type="search" style={{ marginRight: "16px" }} />,
+            <Icon key="1" type="ellipsis" />,
+          ]}
+        >
+          首页
+        </NavBar> */}
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#33A3F4"
+          barTintColor="white"
+          hidden={this.state.hidden}
+        >
+          <TabBar.Item
+            title="首页"
+            key="home"
+            icon={
+              <div
+                style={{
+                  width: "22px",
+                  height: "22px",
+                  background:
+                    "url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat",
+                }}
+              />
+            }
+            selectedIcon={
+              <div
+                style={{
+                  width: "22px",
+                  height: "22px",
+                  background:
+                    "url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat",
+                }}
+              />
+            }
+            selected={this.state.selectedTab === "home"}
+            badge={1}
+            onPress={() => {
+              this.setState({
+                selectedTab: "home",
+              });
+            }}
+            data-seed="logId"
+          >
+            {this.renderContent("首页")}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <div
+                style={{
+                  width: "22px",
+                  height: "22px",
+                  background:
+                    "url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat",
+                }}
+              />
+            }
+            selectedIcon={
+              <div
+                style={{
+                  width: "22px",
+                  height: "22px",
+                  background:
+                    "url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat",
+                }}
+              />
+            }
+            title="分类"
+            key="category"
+            badge={"new"}
+            selected={this.state.selectedTab === "category"}
+            onPress={() => {
+              this.setState({
+                selectedTab: "category",
+              });
+            }}
+            data-seed="logId1"
+          >
+            {this.renderContent("分类")}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <div
+                style={{
+                  width: "22px",
+                  height: "22px",
+                  background:
+                    "url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat",
+                }}
+              />
+            }
+            selectedIcon={
+              <div
+                style={{
+                  width: "22px",
+                  height: "22px",
+                  background:
+                    "url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat",
+                }}
+              />
+            }
+            title="购物车"
+            key="cart"
+            dot
+            selected={this.state.selectedTab === "cart"}
+            onPress={() => {
+              this.setState({
+                selectedTab: "cart",
+              });
+            }}
+          >
+            {this.renderContent("购物车")}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={{
+              uri:
+                "https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg",
+            }}
+            selectedIcon={{
+              uri:
+                "https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg",
+            }}
+            title="我的"
+            key="my"
+            selected={this.state.selectedTab === "my"}
+            onPress={() => {
+              this.setState({
+                selectedTab: "my",
+              });
+            }}
+          >
+            {this.renderContent("我的")}
+          </TabBar.Item>
+        </TabBar>
       </div>
     );
   }
